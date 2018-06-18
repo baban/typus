ee86bc6671847fc93aa9011f5ab58a72e50b3fc6
masanao.matsubara <m_matsubara@okwave.co.jp>
2018-06-18 15:11:46 +0900

    bugfix

--- lib/typus/orm/active_record/search.rb
+++ lib/typus/orm/active_record/search.rb
@@ -32,7 +32,7 @@ module Typus
         end
 
         def build_my_joins(params)
-          query_params = params.to_h.dup
+          query_params = params.dup.permit!.to_h
           query_params.reject! { |k, _| !model_relationships.keys.include?(k.to_sym) }
           query_params.map { |k, _| k.to_sym }
         end
